c7989f5122df0248d34c7e07f0dee984a2396865
ksbains <k1bains@ucsd.edu>
2017-08-30 21:27:02 -0700

    got a lot more done and tested it, however am still getting a bug, need to implement the timer, and need to add the CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,30 +21,49 @@ function Q (text, answers, correct){
 * Qtime
 * timer
 */
-function survey (Qs, userCorrect, Qtime, timer){
+var Q1 = new Q( "what is your name", ["tom", "john", "tobi", "name"], 0);
+var Q2 = new Q( "what is your game", ["bball", "football", "soccer", "baseball"], 0);
+var Q3 = new Q( "what is your lame", ["jquery", "js", "aws", "mongodb"], 2);
+var Qs = [Q1,Q2,Q3];
+
+function survey (Qs){
 	this.questions = Qs;
-	this.question = null;
+	this.question = 0;
 	this.timer = null;
 	this.time = 0;
 	this.saveAnswer = function(ans){
-		this.questoins[this.question].setUserAnser(ans);
+		this.questions[this.question].setUserAnser(ans);
 	}
 	this.setNextQuestion = function(){
 		var next = null;
 		if(this.question === null){// first
 			next = 0;
-		}else if(this.question === this.question.length -1){
+		}else if(this.question === this.question.length -1){//last
 			next = null;
 		}else{
 			next = this.question + 1;
 		}
 		this.question = next;
+	}
+	this.reset = function (){
+		this.questions = Qs;
+		this.question = 0;
+		this.timer = null;
+		this.time = 0;
 	} 
 }
-var Q1 = new Q( "what is your name", ["tom", "john", "tobi", "name"], 0);
 
+var Travia = new survey(Qs);
 // FUNCTIONS 
+function displayResult(){
+	$("#question").empty();
+	$("#question").append('<button class = "button" id="rstbtn">Restart<button>')
+		.on("click", function () {
+			Travia.reset(Qs);
+			displayNextQuestion(Travia.questions[Travia.question]);
+		});
 
+}
 function displayNextQuestion(question){
 	// this is where you have to append all of the elements of the question.
 	// you will start with a an h2 for the question 
@@ -53,22 +72,39 @@ function displayNextQuestion(question){
 	// gotta append subint button too.  
 	// lastly append a button that will have an onclick function. to displayNextQuestion()
 	// 
-	$("#question").empty();
-	$("#question").append('<h2 class="Qh2">' + question.text + '</h2>');
-	appendQuestions(question);
-	$("#question").append('<button class = "button" id="subbtn">Submit<button>')
-	.on("click" function () {
-		displayNextQuestion(question++);
-	});
-
+	if(question.question === null){
+		displayResult()
+	}else{
+		$("#question").empty();
+		console.log(question);
+		console.log("The value of i is: " +i+ " in displayNextQuestion else block");
+		$("#question").append('<h2 class="Qh2">' + question.text + '</h2>');
+		console.log(question);
+		console.log("The value of i is: "+i+ " after appending the H2 ");
+		for(var i = 0; i<question.answers.length; i++){
+			console.log(question);
+			console.log("The value of i is: "+i+ " in the for loop");
+			$("#question").append("<button class='button option'>" + question.answers[i]+ "</button>")
+			.on("click", function() {
+				Travia.saveAnswer(i);
+			});
+		}
+		$("#question").append('<button class = "button" id="subbtn">Submit<button>')
+		.on("click", function () {
+			displayNextQuestion(question++);
+		});
+	}
 }
 function appendQuestions(question){
-	// cycle thru the question and add each optin div.
-	// append each question need the proper code lulz. 
-	for(var i = 0; i<survey.questions.length; i++){
-		$("#question").append('<button class="button option" id=option'+i+'' + question.answers[i]+' </button>')
+	// cycle thru the question and add each optin div
+	console.log("appendQuestions");
+	console.log(question); 
+	for(var i = 0; i<question.answers.length; i++){
+		console.log(question);
+		console.log(i);
+		$("#question").append("<button class='button option'>" + question.answers[i]+ "</button>")
 		.on("click", function() {
-			survey.saveAnswer(i);
+			Travia.saveAnswer(i);
 		});
 	}
 }
@@ -78,7 +114,7 @@ function displayStart() {
 	$("#question").append(
 		$('<button id="startButton" class="button"> Start </button>')
 		.on('click',function() {
-			displayNextQuestion(0);
+			displayNextQuestion(Travia.questions[Travia.question]);
 		})
 	);
 }
